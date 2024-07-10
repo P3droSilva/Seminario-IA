0cf08cad38c267febc153664b44d7d8bd5ad534f
P3dro_Silva <phhinerasky@inf.ufsm.br>
2024-07-10 12:31:32 -0300

    slides

--- Fazendo Previsoes/previsao.py
+++ Fazendo Previsoes/previsao.py
@@ -11,7 +11,7 @@ X = wine.data
 y = wine.target
 
 # Dividir o dataset em conjuntos de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=50)
 
 # Criar e treinar a árvore de decisão
 tree_clf = DecisionTreeClassifier(max_depth=2)
